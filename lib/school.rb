c8c7975578dfbbce765f044d664ed2f5c4157c22
Coral Fussman <coralfussman@gmail.com>
2020-04-01 04:50:07 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,39 @@
-# code here!
+class School
+
+  attr_accessor :name, :roster
+
+def initialize(name)
+  @name = name
+  @roster = {}
+
+end
+
+# def roster
+# @roster = {}
+# end
+
+def add_student(name,grade)
+  if @roster[grade]
+     @roster[grade] << name
+   else
+     @roster[grade] = [name]
+   end
+ end
+
+ def grade(grade)
+   @roster[grade]
+ end
+
+ def sort
+   sorted_hash = {}
+   @roster.sort.each do |grade, name_array|
+     sorted_hash[grade] = (name_array.sort)
+   end
+   sorted_hash
+ end
+end
+
+
+
+school = School.new("Bayside High School")
+school.roster
