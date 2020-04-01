b85536e44c43d24fcd0d4aed4f8b75a8366d3aa4
Coral Fussman <coralfussman@gmail.com>
2020-04-01 04:28:03 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -11,8 +11,8 @@ grade_level_hash = {}
 
 end
 
-def add_student(name,grade)
-  grade_level_hash["new_key"] = []
+def add_student(name,roster)
+grade_level_hash["new_key"] = []
 grade_level_hash["new_key"] << "new_value_for_value_array"
 grade_level_hash
 end
