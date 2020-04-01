84ff73601105cdc8be4411c36300d1780874e2ba
Coral Fussman <coralfussman@gmail.com>
2020-04-01 04:44:04 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -4,11 +4,13 @@ class School
 
 def initialize(name)
   @name = name
+  @roster = {}
+
 end
 
-def roster
-@roster = {}
-end
+# def roster
+# @roster = {}
+# end
 
 def add_student(name,grade)
   if @roster[grade]
